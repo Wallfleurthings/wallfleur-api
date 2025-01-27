60fbd48e39f5d6cf46ae08e8895536c08fe2a5cf
Tohoto T Chophy <tohotochophy@Tohotos-MacBook-Pro.local>
2025-01-28 00:11:26 +0530

    express session

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { 
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000 
     }
